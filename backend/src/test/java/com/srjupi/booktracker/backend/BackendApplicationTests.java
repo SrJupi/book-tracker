95ab48fbc98b30b19bf503a3bcf9359b14d5e307
mint kinhas <lsr.jupi@gmail.com>
2025-09-23 17:43:17 -0300

    bt12: add import to java tests

--- backend/src/test/java/com/srjupi/booktracker/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/srjupi/booktracker/backend/BackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.srjupi.booktracker.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
