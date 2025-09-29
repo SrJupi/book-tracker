5ddc2b01d3420c66bba0cd0e844c3879be6d3d4c
mint kinhas <lsr.jupi@gmail.com>
2025-09-29 10:38:41 -0300

    bt38 - basic userEntity and schema

---
+++ backend/src/main/java/com/srjupi/booktracker/backend/users/UserEntity.java
@@ -0,0 +1,18 @@
+package com.srjupi.booktracker.backend.users;
+
+import com.srjupi.booktracker.backend.common.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
