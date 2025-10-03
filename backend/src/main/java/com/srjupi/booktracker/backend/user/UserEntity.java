cda4334bdd491016d9ba4b62d9f0f925176fa565
mint kinhas <lsr.jupi@gmail.com>
2025-10-03 13:50:34 -0300

    b42 - entity coverage

--- backend/src/main/java/com/srjupi/booktracker/backend/user/UserEntity.java
+++ backend/src/main/java/com/srjupi/booktracker/backend/user/UserEntity.java
@@ -3,12 +3,16 @@ package com.srjupi.booktracker.backend.user;
 import com.srjupi.booktracker.backend.common.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity extends BaseEntity {
 
     @Column(nullable = false, unique = true)
